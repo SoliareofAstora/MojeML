edb2a2dcc31a6723aa2bf080985a8376b3f871f6
Piotr Kucharski <piotr1kucharski@gmail.com>
2018-03-02 18:15:22 +0100

    finished friday assigment

--- DataLanguages/Cw1.py
+++ DataLanguages/Cw1.py
@@ -4,7 +4,6 @@ for x in range(7):
     arr[x,:] = [1 + x, 9 - x, 2 ** (2 + x)]
 
 
-
 # x = 10
 # a = [2 + i * 2 for i in range(x)]
 # b = [8 - i * 2 for i in range(x)]
@@ -15,7 +14,9 @@ arr[[4,6],:].sum()
 
 arr[:,2]
 
-arr[2],arr[3] = arr[3],arr[2]
+temp = np.copy(arr[3])
+arr[3]= arr[2]
+arr[2]=temp
 
 arr = np.zeros((3,5))
 for x in range(5):
@@ -25,5 +26,3 @@ for x in range(5):
 arr = np.zeros((6,3))
 for x in range(6):
     arr[x,:]=x
-
-b
