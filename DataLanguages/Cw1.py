b4700096e6379db0cf4017e3c6afe20bd1762b73
Piotr Kucharski <piotr1kucharski@gmail.com>
2018-03-02 18:01:16 +0100

    cw1

---
+++ DataLanguages/Cw1.py
@@ -0,0 +1,29 @@
+import numpy as np #you can remove numpy
+arr = np.zeros((7,3))
+for x in range(7):
+    arr[x,:] = [1 + x, 9 - x, 2 ** (2 + x)]
+
+
+
+# x = 10
+# a = [2 + i * 2 for i in range(x)]
+# b = [8 - i * 2 for i in range(x)]
+# c = [1 / (i + 1) for i in range(x)]
+# d = [i/(i+1) for i in range(x)]
+
+arr[[4,6],:].sum()
+
+arr[:,2]
+
+arr[2],arr[3] = arr[3],arr[2]
+
+arr = np.zeros((3,5))
+for x in range(5):
+    arr[:,x]=x+1
+
+
+arr = np.zeros((6,3))
+for x in range(6):
+    arr[x,:]=x
+
+b
