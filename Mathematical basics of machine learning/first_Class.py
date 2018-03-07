425464a6dcfc82808179081a75a5f47953181f27
Piotr Kucharski <piotr1kucharski@gmail.com>
2018-03-07 17:06:41 +0100

    MPUM

--- Mathematical basics of machine learning/first_Class.py
+++ Mathematical basics of machine learning/first_Class.py
@@ -13,6 +13,7 @@ def sort(lista):
         if sorted:
             break
 
+
 x = [1, 6, 4, 2, 7, 9, 4, 2, 7, 4, 32]
 x = x * 2
 sort(x)
@@ -26,36 +27,24 @@ x = (x == 1)
 x = np.random.randint(0, 10, (5, 5))
 x = (x % 2 == 0)
 
-
 # Z wektora [1 .. 10] zastąp parzyste elementy przez 0
 x = np.random.randint(0, 100, 100)
-x = np.where(x % 2 == 0,0,x)
+x = np.where(x % 2 == 0, 0, x)
 
 # Połącz ze sobą 2 wektory horyzontalniePołącz ze sobą 2 wektory horyzontalnie
 x = np.random.randint(0, 100, 10)
-x = np.append(x,x)
-x=np.array([x,x])
+x = np.append(x, x)
+x = np.array([x, x])
 
 # Utwórz [1 .. 100 ] rozmiaru 10 x 10
-x = np.arange(0,100)
-x = x.reshape((10,10))
+x = np.arange(0, 100)
+x = x.reshape((10, 10))
 
+# W losowej macierzy 10 x 10 zamień ze sobą 0 i 3 kolumnę (slicing)
+x = x[:,[2,1,0,3,4,5,6,7,8,9]]
 
-
-# # Zadania: numpy1 + pkt (za zrobienie większości)
-#
-# Jeśli macierz nie jest określona przyjmij macierz dowolnego rozmiaru
-#
-# 1. Znormalizuj (średnia 0, wariancja 1) losowa macierz 5x5
-
-# 1.
-# 1.
-# 1. Znajdź wspólne elementy w 2 losowych wektorach 10 x 1 (intersect1d)
-# 1. W losowej macierzy 10 x 10 zamień ze sobą 0 i 3 kolumnę (slicing)
-# 1. Wypisz wektor 20 x 1, wypisują wszystkie liczby (bez ucinania) (np.set_printoptions)
-# 1. Wygeneruj macierz floatów w przedziale 4x4
-
-
+# Wygeneruj macierz floatów w przedziale 4x4
+x = np.random.uniform(0,10,(4,4))
 
 # # Zadania: numpy2 + pkt (za zrobienie większości)
 # 1. Odejmij od każdej kolumny średnią kolumny
@@ -63,15 +52,5 @@ x = x.reshape((10,10))
 # 3. W macierzy 20x20 znajdź sumę w bloków, rozmiar bloku (5x5
 # Więcej zadań (może częśc odpowiedzi nawet):
 #
-# 1. https://www.labri.fr/perso/nrougier/teaching/numpy.100/index.html
-# 1. https://www.machinelearningplus.com/101-numpy-exercises-python/
-
-
-
-
-
-
-
-
-
-
+#  https://www.labri.fr/perso/nrougier/teaching/numpy.100/index.html
+#  https://www.machinelearningplus.com/101-numpy-exercises-python/
