d3c2c59aeb8cfc91a956cf20408c337a4a1f22a7
Piotr Kucharski <piotr1kucharski@gmail.com>
2018-04-24 18:59:31 +0200

    first attempt in matlab :D

---
+++ DataLanguages/MATLAB/firstMATLAB.m
@@ -0,0 +1,15 @@
+a = [2,3,1,1,1];
+[suma,iloczyn] = suma_iloczyn(a)
+
+function [suma,iloczyn] = suma_iloczyn(x) 
+    suma = sum(x);
+    iloczyn=1.;
+    for i=1:length(x)
+        iloczyn = iloczyn * x(i);
+    end
+end
+
+
+
+
+
